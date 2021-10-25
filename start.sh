cabb7213b04819794ea7580fef8d11dc22b57bb4
Lukáš Mázl <lukasmazl@seznam.cz>
2021-10-25 14:51:07 +0200

    Added start script

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+DIR=OS-Process-Watcher
+MVNW_FILE="mvnw"
+JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java
+
+if [ -d "$DIR" ]; then
+    echo "Git already cloned"
+else
+    git clone https://github.com/LukasMazl/OS-Process-Watcher
+fi
+
+cd "./$DIR/"
+
+#chmod 777 ./mvnw
+echo `pwd`
+chmod +x "$MVNW_FILE"
+
+if [ -d "target" ]; then
+	echo "Target already exists"
+else
+	/bin/sh "$MVNW_FILE" "clean" "install"
+fi
+
+clear
+
+"$JAVA_HOME" -jar ./target/OperationSystems-1.0-SNAPSHOT.jar -d -master -tasksFile,Socket,Memory
